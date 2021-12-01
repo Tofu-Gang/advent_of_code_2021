a0eede15149ab03f112c29d602b35bf35ef5dc8c
Tofu Gang <tofugangsw@gmail.com>
2021-12-02 00:39:14 +0100

    Initial commit.

---
+++ main.py
@@ -0,0 +1,32 @@
+__author__ = "Tofu Gang"
+__email__ = "tofugangsw@gmail.com"
+
+# from day_xx.day_xx import puzzle_1 as puzzle
+
+"""
+You're minding your own business on a ship at sea when the overboard alarm goes 
+off! You rush to see if you can help. Apparently, one of the Elves tripped and 
+accidentally sent the sleigh keys flying into the ocean!
+
+Before you know it, you're inside a submarine the Elves keep ready for 
+situations like this. It's covered in Christmas lights (because of course it 
+is), and it even has an experimental antenna that should be able to track the 
+keys if you can boost its signal strength high enough; there's a little meter 
+that indicates the antenna's signal strength by displaying 0-50 stars.
+
+Your instincts tell you that in order to save Christmas, you'll need to get all 
+fifty stars by December 25th.
+
+Collect stars by solving puzzles. Two puzzles will be made available on each day 
+in the Advent calendar; the second puzzle is unlocked when you complete the 
+first. Each puzzle grants one star. Good luck!
+"""
+
+################################################################################
+
+if __name__ == "__main__":
+    print("---Advent Of Code 2021---")
+    # puzzle()
+    exit(0)
+
+################################################################################
